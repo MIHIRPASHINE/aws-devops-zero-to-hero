046d93ee9e4b8fb6144761fcb39fec2d460674c1
MihirPashine <138210028+MIHIRPASHINE@users.noreply.github.com>
2024-08-20 02:23:25 +0530

    Update app.py

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, this project is implemented to brush up on the AWS managed CI/CD services!'
+    return 'Hello, this project is implemented to brush up on the AWS managed CI/CD services!!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
